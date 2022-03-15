aaff6b17dc196dba8041fe47a15230f01f8713f8
brie <briememo95@gmail.com>
2022-03-15 14:28:41 +0300

    initial Commit

---
+++ script.js
@@ -0,0 +1,108 @@
+// $(document).ready(function(){
+
+//   $('.card-1').mouseover(function(){
+//     $('.card-1').css({"font-size": "180%","opacity": "50%"});
+//     $("#card-text-1").show();
+// }).mouseout(function(){
+//     $('.card-1').css("opacity","100%");
+//     $("#card-text-1").hide();
+// });
+// $('.card-2').mouseover(function(){
+// $('#card-text-2').css({"background":"#3A3D4A"});
+// $('.card-2').css({"font-size": "200%","opacity": "50%"});
+// $("#card-text-2").show();
+// }).mouseout(function(){
+// $('.card-2').css("opacity","100%");
+// $("#card-text-2").hide();
+// });
+// $('.card-3').mouseover(function(){
+// $('.card-3').css({"font-size": "200%","opacity": "50%"});
+// $("#card-text-3").show();
+// }).mouseout(function(){
+// $('.card-3').css("opacity","100%");
+// $("#card-text-3").hide();
+// });
+// $('.card-4').mouseover(function(){
+// $('.card-4').css({"font-size": "180%","opacity": "50%"});
+// $("#card-text-4").show();
+// }).mouseout(function(){
+// $('.card-4').css("opacity","100%");
+// $("#card-text-4").hide();
+// });
+// $('.card-5').mouseover(function(){
+// $('#card-text-5').css({"background":"#fff", "color":"black"});
+// $('.card-5').css({"font-size": "180%","opacity": "50%"});
+// $("#card-text-5").show();
+// }).mouseout(function(){
+// $('.card-5').css("opacity","100%");
+// $("#card-text-5").hide();
+// });
+// $('.card-6').mouseover(function(){
+// $('#card-text-6').css({"background":"#fff", "color":"black"});
+// $('.card-6').css({"font-size": "180%","opacity": "50%"});
+// $("#card-text-6").show();
+// }).mouseout(function(){
+// $('.card-6').css("opacity","100%");
+// $("#card-text-6").hide();
+// });
+// $('.card-7').mouseover(function(){
+// $('.card-7').css({"font-size": "200%","opacity": "50%"});
+// $("#card-text-7").show();
+// }).mouseout(function(){
+// $('.card-7').css("opacity","100%");
+// $("#card-text-7").hide();
+// });
+// $('.card-8').mouseover(function(){
+// $('.card-8').css({"font-size": "200%","opacity": "50%"});
+// $("#card-text-8").show();
+// }).mouseout(function(){
+// $('.card-8').css({"opacity": "100%"});
+// $("#card-text-8").hide();
+// });
+// $("#designdesc").click(function(){
+// $("#designdetails").show()
+//   $("#designdesc").hide('fast')
+// });
+// $("#devdesc").click(function(){
+//   $("#devdetails").show()
+//     $("#devdesc").hide('fast')
+//   });
+//   $("#productdesc").click(function(){
+//     $("#productdetails").show()
+//       $("#productdesc").hide('fast')
+//     });
+
+// $("#submit").click(function () {
+// var FullName = $("#fname").val();
+// var Email = $("#email").val();
+// var Message = $("#message").val();
+// if (FullName == '' || Email == '' || Message == '') {
+// alert("Fill all fields");
+// } else {
+// alert(FullName + " Your message has been Successfully Submitted!");
+// }
+// })
+// event.preventDefault();
+   
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
